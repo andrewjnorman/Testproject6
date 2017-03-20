ff16e6d211412ed3bf77aa8197e95c89a0e7e9fd
andrewjnorman <andrewjnorman@live.com>
2017-03-20 15:11:22 +0000

    bollocks

---
+++ TeamProject5/TeamProject5/TeamProject5.cpp
@@ -0,0 +1,12 @@
+// TeamProject5.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	printf("I'm so glad Microsoft doesn't build aeroplanes\n");
+    return 0;
+}
+
