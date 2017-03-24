1471614a74f6b893d473a85256c765cb05bbc91f
andrewjnorman <andrewjnorman@live.com>
2017-03-24 14:00:22 +0000

    added google test

---
+++ PlayTest_gtest/PlayTest_gtest/PlayTest_gtest.cpp
@@ -0,0 +1,21 @@
+// PlayTest_gtest.cpp : Defines the entry point for the console application.
+
+
+#include "stdafx.h"
+#include <gtest/gtest.h>
+
+static int x = 1;
+
+TEST(MyTest, FirstTests)
+{
+	EXPECT_EQ(2, x);
+}
+
+int main(int argc, char* argv[])
+{
+
+	testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
+
---
+++ PlayTest_gtest/PlayTest_gtest/class1.h
@@ -0,0 +1,10 @@
+#pragma once
+class class1
+{
+public:
+	class1();
+	~class1();
+	int x;
+	int fn_1(void);
+};
+
